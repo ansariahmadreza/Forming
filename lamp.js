b7458a1caeada93901c1c378cf4fd0a1bc047f0a
ahmadreza <ahmadreza.an2001@gmail.com>
2024-12-27 00:28:32 +0330

    Add files via upload

--- lamp.js
+++ lamp.js
@@ -8,17 +8,21 @@ let imgNew=document.getElementById('image')
 
 let lamp = true
 
+let btn=document.getElementById('btn')
+
 function clickfunc() {
 
     if (lamp) {
         lamp = false
         imgNew.setAttribute('src','images/pic07.jpg ')
+        btn.innerHTML='turnOff'
 
 
 
     } else {
 
         lamp = true
+        btn.innerHTML='turnOn'
         imgNew.setAttribute('src','images/pic03.jpg ')
 
 
