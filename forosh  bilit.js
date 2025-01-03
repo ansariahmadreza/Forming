b4e2f36314b70fa59fa7ef61438c2c7496d23ecc
ahmadreza <ahmadreza.an2001@gmail.com>
2025-01-04 02:35:09 +0330

    Add files via upload

--- forosh  bilit.js
+++ forosh  bilit.js
@@ -1,3 +1,4 @@
+///////////   I wrote the code explanation myself.   /////////////////
 const boxBilit = {
 
     fars: ['shiraz', 'fasa', 'noorabad', 'eqlid', 'jahrom'],
@@ -5,24 +6,24 @@ const boxBilit = {
     tehran: ['theran', 'varamin', 'damavand']
 }
 
-let cityMe = document.querySelector('.cityMe')
-let destination = document.querySelector('.destination')
+let cityMe = document.querySelector('.cityMe') /// فراخوانی اسم استان ها از کد html
+let destination = document.querySelector('.destination') /// فراخوانی اسم شهر ها از کد html
 
 
-cityMe.addEventListener('change', changeCity)
+cityMe.addEventListener('change', changeCity) // تنظیم انجام رویداد و نوع ان
 
 function changeCity() {
 
-    let selectCity = cityMe.value
+    let selectCity = cityMe.value /// اسم استان درون متغییر ریخته میشه
 
     if (selectCity === "please select") {
         destination.innerHTML = '<option>Please select a city</option>'
-        return
+        return /// در صورت انجام مراحل قبل کد متوقف میشه
     } else {
 
-        let cities = boxBilit[selectCity]
+        let cities = boxBilit[selectCity] /// با انتخاب استان اسم شهر ها مشخص میشه 
 
-        destination.innerHTML = ''
+        destination.innerHTML = '' /// خالی کردن لیست بعد از تغییر اسم شهر
 
         cities.forEach(function (city) {
             destination.innerHTML += '<option>' + city + '</option>';  // اضافه کردن شهرها به لیست
