bd543abffbd996cc9573ce2e1301466951837a4a
ahmadreza <ahmadreza.an2001@gmail.com>
2025-03-27 16:34:42 +0330

    Add files via upload

--- show time.js
+++ show time.js
@@ -7,22 +7,43 @@ const second = document.querySelector('.second')
 setInterval(function () {
 
     let myTime = new Date()
-
+    timeHours = myTime.getHours()
+    timeMin = myTime.getMinutes()
     timeSec = myTime.getSeconds()
 
-    hours.innerHTML = myTime.getHours() + ' ' + ':'
-    minutes.innerHTML = myTime.getMinutes() + ' ' + ':'
+    hours.innerHTML = timeHours + ' ' + ':'
+    minutes.innerHTML = timeMin + ' ' + ':'
 
 
     if (timeSec < 10) {
 
 
         second.innerHTML = '0' + timeSec
-    }else{
-        
+    } else {
+
         second.innerHTML = timeSec
     }
-   
+
+
+    if (timeMin < 10) {
+
+
+        second.innerHTML = '0' + timeMin
+    } else {
+
+        second.innerHTML = timeMin
+    }
+
+
+    if (timeHours < 10) {
+
+
+        second.innerHTML = '0' + timeHours
+    } else {
+
+        second.innerHTML = timeHours
+    }
+
     console.log('هر یک ثانیه')/// برای امتحان کردن درستی سینتکس setinterval
 
 }, 1000)
