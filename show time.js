721b99fc70ea0c6cfe650a8c664a907d71882ee4
ahmadreza <ahmadreza.an2001@gmail.com>
2025-03-27 12:49:56 +0330

    Add files via upload

---
+++ show time.js
@@ -0,0 +1,29 @@
+const hours = document.querySelector('.hours')
+const minutes = document.querySelector('.minutes')
+const second = document.querySelector('.second')
+
+
+
+setInterval(function () {
+
+    let myTime = new Date()
+
+    hours.innerHTML = myTime.getHours() + ' ' + ':'
+    minutes.innerHTML = myTime.getMinutes() + ' ' +':'
+    second.innerHTML = myTime.getSeconds()
+
+
+
+    console.log('هر یک ثانیه')/// برای امتحان کردن درستی سینتکس setinterval
+
+}, 1000)
+
+
+//console.log(myTime)
+// console.log(myTime.getFullYear())
+// console.log(myTime.getMonth())
+// console.log(myTime.getDate())
+// console.log(myTime.getHours())
+// console.log(myTime.getMinutes())
+// console.log(myTime.getSeconds())
+// console.log(myTime.getDay())
